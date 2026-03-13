0a1594b5c769665deeba599dc9061d5133a4cee5
Thomas Leeds <thomas.leeds@nhsbsa.nhs.uk>
2026-03-13 13:08:18 +0000

    Journey A Publish

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   // Service name
-  serviceName: 'Receiving your public service pensions remedy choice letter',
+  serviceName: 'Receiving your public service pensions remedy pack',
 
   // Port to run the prototype on locally
   port: 3000
